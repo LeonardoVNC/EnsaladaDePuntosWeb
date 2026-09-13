57c311a37cdfa15ba89570a39ab3594ee606ffbc
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2026-09-12 23:57:55 -0400

    chore[proxy]: enable proxy server for http on vite

--- EnsaladaFront/vite.config.ts
+++ EnsaladaFront/vite.config.ts
@@ -6,5 +6,14 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: '../server/public'
-  }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
